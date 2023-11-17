325845f0af82a0c86a90fea3aeed13a5b24709c4
ravenbarajas057 <ravenbarajas2001@gmail.com>
2023-11-17 18:08:09 +0800

    Added basic animation and effects

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,69 +1,63 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Skills.css';
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import softSkill1 from '../assets/skills/softSkill1.jpg';
+import softSkill2 from '../assets/skills/softSkill2.jpg';
+import softSkill3 from '../assets/skills/softSkill3.jpg';
+import softSkill4 from '../assets/skills/softSkill4.jpg';
+import softSkill5 from '../assets/skills/softSkill5.jpg';
 
 function Skills() {
-  return (
-    <div className="carousel-section">
-        <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <p>Soft Skills</p>
-            <Swiper
-              className="mySwiper2 swiper-v"
-              direction={'vertical'}
-              spaceBetween={50}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-            >
-              <SwiperSlide>Vertical Slide 1</SwiperSlide>
-              <SwiperSlide>Vertical Slide 2</SwiperSlide>
-              <SwiperSlide>Vertical Slide 3</SwiperSlide>
-            </Swiper>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>Technical Skills</p>
-            <Swiper
-              className="mySwiper2 swiper-v"
-              direction={'vertical'}
-              spaceBetween={50}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-            >
-              <SwiperSlide>Vertical Slide 1</SwiperSlide>
-              <SwiperSlide>Vertical Slide 2</SwiperSlide>
-              <SwiperSlide>Vertical Slide 3</SwiperSlide>
-            </Swiper>
-        </SwiperSlide>
-      </Swiper>
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    // Additional logic or side effects if needed
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+  return (  
+    <div className="skills-container">
+        <div className="soft-skill-heading">
+          <p>People Skills</p>
+        </div>
+      <div className="skills-section"> 
+        <div className="soft-skill" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={softSkill1}/>
+          {isHovered && <div className="soft-skill-desc">
+            <p className='skill-heading'>Team player</p>
+            </div>}
+        </div>
+        <div className="soft-skill" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={softSkill2}/>
+          {isHovered && <div className="soft-skill-desc">
+            <p className='skill-heading'>Effective Leader</p>
+            </div>}
+        </div>
+        <div className="soft-skill" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={softSkill3}/>
+          {isHovered && <div className="soft-skill-desc">
+            <p className='skill-heading'>Strategic Time Manager</p>
+            </div>}
+        </div>
+        <div className="soft-skill" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={softSkill4}/>
+          {isHovered && <div className="soft-skill-desc">
+            <p className='skill-heading'>Problem Solver</p>
+            </div>}
+        </div>
+        <div className="soft-skill" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <img src={softSkill5}/>
+          {isHovered && <div className="soft-skill-desc">
+            <p className='skill-heading'>Critical Thinker</p>
+            </div>}
+        </div>
+      </div>
     </div>
   );
 }
