f822b591ae414b749ccf4d65a4c4f20756a4124f
ravenbarajas057 <ravenbarajas2001@gmail.com>
2023-12-06 15:24:12 +0800

    Added Icons in Tech Skill

--- src/pages/TechSkill.js
+++ src/pages/TechSkill.js
@@ -5,23 +5,30 @@ import html5Tech from '../assets/skills/html5Tech.png';
 import css3Tech from '../assets/skills/css3Tech.png';
 import jsTech from '../assets/skills/jsTech.png';
 import reactTech from '../assets/skills/reactTech.png';
+import bootstrapTech from '../assets/skills/reactTech.png';
+import typescriptTech from '../assets/skills/typescriptTech.png';
+import figmaTech from '../assets/skills/figmaTech.png';
 
-import pythonTech from '../assets/skills/pythonTech.png';
+import djangoTech from '../assets/skills/djangoTech.png';
+import laravelTech from '../assets/skills/laravelTech.png';
 import phpTech from '../assets/skills/phpTech.png';
-import mysqlTech from '../assets/skills/mysqlTech.png';
 import firebaseTech from '../assets/skills/firebaseTech.png';
 
 import xamarinTech from '../assets/skills/xamarinTech.png';
+import kotlinTech from '../assets/skills/kotlinTech.png';
 import flutterTech from '../assets/skills/flutterTech.png';
 import dotnetTech from '../assets/skills/dotnetTech.png';
 
 import csharpTech from '../assets/skills/csharpTech.png';
 import javaTech from '../assets/skills/javaTech.png';
+import mysqlTech from '../assets/skills/mysqlTech.png';
 
 import trelloTech from '../assets/skills/trelloTech.png';
 import clickupTech from '../assets/skills/clickupTech.png';
+import jiraTech from '../assets/skills/jiraTech.png';
 import gitTech from '../assets/skills/gitTech.png';
 
+import pythonTech from '../assets/skills/pythonTech.png';
 import numpyTech from '../assets/skills/numpyTech.png';
 import tableuTech from '../assets/skills/tableuTech.png';
 
@@ -46,6 +53,9 @@ function TechSkill() {
                 <div className="tech-grid-item"><img src={css3Tech} alt="CSS3" /></div>
                 <div className="tech-grid-item"><img src={jsTech} alt="JavaScript" /></div>
                 <div className="tech-grid-item"><img src={reactTech} alt="React" /></div>
+                <div className="tech-grid-item"><img src={bootstrapTech} alt="bootstrapTech" /></div>
+                <div className="tech-grid-item"><img src={figmaTech} alt="figmaTech" /></div>
+                <div className="tech-grid-item"><img src={typescriptTech} alt="typescriptTech" /></div>
               </div>
             </div>
             <div className="technical-skill-card-2">
@@ -53,7 +63,8 @@ function TechSkill() {
                 <p>Backend Development</p>
               </div>
               <div className='tech-skill-desc'>
-                <div className="tech-grid-item"><img src={pythonTech} alt="pythonTech" /></div>
+                <div className="tech-grid-item"><img src={djangoTech} alt="djangoTech" /></div>
+                <div className="tech-grid-item"><img src={laravelTech} alt="laravelTech" /></div>
                 <div className="tech-grid-item"><img src={phpTech} alt="phpTech" /></div>
                 <div className="tech-grid-item"><img src={firebaseTech} alt="firebaseTech" /></div>
               </div>
@@ -64,6 +75,7 @@ function TechSkill() {
                 </div>
                 <div className='tech-skill-desc'>
                   <div className="tech-grid-item"><img src={xamarinTech} alt="xamarinTech" /></div>
+                  <div className="tech-grid-item"><img src={kotlinTech} alt="kotlinTech" /></div>
                   <div className="tech-grid-item"><img src={flutterTech} alt="flutterTech" /></div>
                   <div className="tech-grid-item"><img src={dotnetTech} alt="dotnetTech" /></div>
                 </div>
@@ -87,6 +99,7 @@ function TechSkill() {
               <div className='tech-skill-desc'>
                 <div className="tech-grid-item"><img src={trelloTech} alt="trelloTech" /></div>
                 <div className="tech-grid-item"><img src={gitTech} alt="gitTech" /></div>
+                <div className="tech-grid-item"><img src={jiraTech} alt="jiraTech" /></div>
                 <div className="tech-grid-item"><img src={clickupTech} alt="clickupTech" /></div>
               </div>
             </div>
@@ -95,6 +108,7 @@ function TechSkill() {
                 <p>Data Analytics</p>
               </div>
               <div className='tech-skill-desc'>
+                <div className="tech-grid-item"><img src={pythonTech} alt="pythonTech" /></div>
                 <div className="tech-grid-item"><img src={numpyTech} alt="numpyTech" /></div>
                 <div className="tech-grid-item"><img src={tableuTech} alt="tableuTech" /></div>
               </div>
