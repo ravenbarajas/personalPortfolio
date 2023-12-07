c7bd84024b111c104285e6cdf79761deb739defb
ravenbarajas057 <ravenbarajas2001@gmail.com>
2023-12-07 21:27:24 +0800

    Added a Swiper Slide to Soft Skill page

--- src/pages/PeopleSkill.js
+++ src/pages/PeopleSkill.js
@@ -1,6 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './PeopleSkill.css';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
+
+import seemore from '../assets/seemore.png';
+
 function PeopleSkill() {
   useEffect(() => {
     // Additional logic or side effects if needed
@@ -12,72 +25,117 @@ function PeopleSkill() {
           <p>People Skills</p>
         </div>
         <div className="soft-skills-section"> 
-          <div className='soft-skills-row'>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-1">
-                <div className='soft-skill-logo-subheader-wbg'>
-                    <p>Passionate</p>
-                </div>
-                <div className='soft-skill-logo-header-wbg'>
-                    <p>Leader.</p>
-                </div>
-              </div>
-              
+            <div className='soft-skills-outer-row'>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={0}
+                    freeMode={true}
+                    modules={[FreeMode, Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <div className='soft-skills-inner-row'>
+                            <div className="soft-skill">
+                                <div className="soft-skill-logo-1">
+                                    <div className='soft-skill-header-container'>
+                                        <div className='soft-skill-logo-subheader-wbg'>
+                                            <p>Passionate</p>
+                                        </div>
+                                        <div className='soft-skill-logo-header-wbg'>
+                                            <p>Leader.</p>
+                                        </div>
+                                    </div>
+                                    <div className='soft-skill-logo-card-seemore'>
+                                    <img src={seemore} alt="seemore" />
+                                    </div>
+                                </div>
+                            </div> 
+                            <div className="soft-skill">
+                                <div className="soft-skill-logo-2">
+                                    <div className='soft-skill-header-container'>
+                                        <div className='soft-skill-logo-subheader-wbg'>
+                                            <p>Effective</p>
+                                        </div>
+                                        <div className='soft-skill-logo-header-wbg'>
+                                            <p>Communicator.</p>
+                                        </div>
+                                    </div>
+                                    <div className='soft-skill-logo-card-seemore'>
+                                    <img src={seemore} alt="seemore" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="soft-skill">
+                                <div className="soft-skill-logo-3">
+                                    <div className='soft-skill-header-container'>
+                                        <div className='soft-skill-logo-subheader'>
+                                            <p>Efficient</p>
+                                        </div>
+                                        <div className='soft-skill-logo-header'>
+                                            <p>Time Manager.</p>
+                                        </div>
+                                    </div>
+                                    <div className='soft-skill-logo-card-seemore'>
+                                    <img src={seemore} alt="seemore" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="soft-skill">
+                                <div className="soft-skill-logo-4">
+                                    <div className='soft-skill-header-container'>
+                                        <div className='soft-skill-logo-subheader-wbg'>
+                                            <p>Innovative</p>
+                                        </div>
+                                        <div className='soft-skill-logo-header-wbg'>
+                                            <p>Problem Solver.</p>
+                                        </div>
+                                    </div>
+                                    <div className='soft-skill-logo-card-seemore'>
+                                    <img src={seemore} alt="seemore" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="soft-skill">
+                            <div className="soft-skill-logo-5">
+                                <div className='soft-skill-header-container'>
+                                    <div className='soft-skill-logo-subheader-wbg'>
+                                        <p>Precise</p>
+                                    </div>
+                                    <div className='soft-skill-logo-header-wbg'>
+                                        <p>Attention to Detail.</p>
+                                    </div>
+                                </div>
+                                <div className='soft-skill-logo-card-seemore'>
+                                <img src={seemore} alt="seemore" />
+                                </div>
+                            </div>
+                            </div>
+                            <div className="soft-skill">
+                            <div className="soft-skill-logo-6">
+                                <div className='soft-skill-header-container'>
+                                    <div className='soft-skill-logo-subheader'>
+                                        <p>Strategic</p>
+                                    </div>
+                                    <div className='soft-skill-logo-header'>
+                                        <p>Critical Thinker.</p>
+                                    </div>
+                                </div>
+                                <div className='soft-skill-logo-card-seemore'>
+                                <img src={seemore} alt="seemore" />
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide></SwiperSlide>
+                    <SwiperSlide></SwiperSlide>
+                    <SwiperSlide></SwiperSlide>
+                    <SwiperSlide></SwiperSlide>
+                </Swiper>
             </div>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-2">
-                <div className='soft-skill-logo-subheader-wbg'>
-                    <p>Effective</p>
-                </div>
-                <div className='soft-skill-logo-header-wbg'>
-                    <p>Communicator.</p>
-                </div>
-              </div>
-            </div>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-3">
-                <div className='soft-skill-logo-subheader'>
-                    <p>Innovative</p>
-                </div>
-                <div className='soft-skill-logo-header'>
-                    <p>Problem Solver.</p>
-                </div>
-              </div>
-            </div>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-4">
-                <div className='soft-skill-logo-subheader-wbg'>
-                    <p>Strategic</p>
-                </div>
-                <div className='soft-skill-logo-header-wbg'>
-                    <p>Time Manager.</p>
-                </div>
-              </div>
-            </div>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-5">
-                <div className='soft-skill-logo-subheader-wbg'>
-                    <p>Precise</p>
-                </div>
-                <div className='soft-skill-logo-header-wbg'>
-                    <p>Attention to Detail.</p>
-                </div>
-              </div>
-            </div>
-            <div className="soft-skill">
-              <div className="soft-skill-logo-6">
-                <div className='soft-skill-logo-subheader'>
-                    <p>Precise</p>
-                </div>
-                <div className='soft-skill-logo-header'>
-                    <p>Attention to Detail.</p>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
