6d4bf5861f6e0f3d270e4fdd8471563d9a9ca3a9
ravenbarajas057 <ravenbarajas2001@gmail.com>
2023-12-10 17:19:56 +0800

    Revised Homepage Design to Card Based

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,57 +14,71 @@ function HomePage() {
   return (
     <div>
       <div className="homepage-container">
-        <div className="leftColumnStyle">
-          <div className="blob-frame">
-            <img
-                src={yourImage}
-                alt="Your Image"
-                className="imageStyle"
-            />
+        <div className='homepage-col-container'>
+          <div className="left-col-container">
+            <div className="left-col">
+              <div className="blob-frame-card">
+                <div className="blob-frame">
+                  <img src={yourImage} className="imageStyle"/>
+                </div>
+              </div>
+              <div className="left-col-contact-card">
+                <div className="left-col-contact">
+                  <img src={githubIcon} className="githubIcon"/>
+                  <img src={linkedinIcon} className="linkedinIcon"/>
+                  <img src={fbIcon} className="fbIcon"/>
+                </div>
+              </div>
             </div>
-          <p className="greeting-heading">Hey, I'm Carl Raven.</p>
-          <p className="greeting-heading-role">Software Developer</p>
-          <p className="greeting-subheading">Committed to producing top-notch results.</p>
-          <div className='contact-icon'>
-            <img src={githubIcon}/>
-            <img src={linkedinIcon}/>
-            <img src={fbIcon}/>
+          </div>
+          <div className="middle-col-container">
+            <div className="middle-col">
+              <div className="greeting-card">
+                <div className="greeting-heading">
+                  <p>Hey, I'm Carl Raven.</p>
+                </div>
+                <div className="greeting-heading-card">
+                  <p className="greeting-heading-role-1">Software</p>
+                  <p className="greeting-heading-role-2">Developer</p>
+                </div>
+              </div>
+              <div className="aboutme-card">
+                <div className="aboutme-heading">
+                  <p>About me</p>
+                </div>
+                <div className="aboutme-subheading-card">
+                  <div className="aboutme-subheading">
+                    <p>IT graduate from Polytechnic University of the Philippines with a strong coding passion and problem-solving acumen, possessing a solid software development background, quick learner, and detail-oriented.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right-col-container">
+            <div className="right-col">
+              <div className="techskill-count-card">
+                <div className="techskill-count-heading">
+                  <p>25+</p>
+                </div>
+                <div className="techskill-count-subheading-card">
+                  <div className="techskill-count-subheading">
+                    <p>Technology Skills</p>
+                  </div>
+                </div>
+              </div>
+              <div className="proj-count-card">
+                <div className="proj-count-heading">
+                  <p>10+</p>
+                </div>
+                <div className="proj-count-subheading-card">
+                  <div className="proj-count-subheading">
+                    <p>Projects Made</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        {/* <div className="rightColumnStyle">
-
-        <div className='left-side-col'>
-          <div className="nav-bubble">
-            <div className="nav-bubble-logo-3">
-              <img src={techskillBubble}/>
-              <p>Skills</p>
-            </div>
-          </div>
-          <div className="nav-bubble">
-            <div className="nav-bubble-logo-5">
-              <img src={experienceBubble}/>
-              <p>Experience</p>
-            </div>
-          </div>
-        </div>
-
-        <div className='right-side-col'>
-          <div className="nav-bubble">
-            <div className="nav-bubble-logo-4">
-              <img src={projectsBubble}/>
-              <p>Projects</p>
-            </div>
-          </div>
-          <div className="nav-bubble">
-            <div className="nav-bubble-logo-6">
-              <img src={educationBubble}/>
-              <p>Education</p>
-            </div>
-          </div>
-        </div>
-
-        </div> */}
-
       </div>
     </div>
   );
