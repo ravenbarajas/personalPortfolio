810529ec3c1a15a8b67de839458b8cdffde29b9d
ravenbarajas057 <ravenbarajas2001@gmail.com>
2024-08-05 23:18:21 +0800

    Updated Experience Section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -79,6 +79,161 @@ function Experience() {
                         </div>
                     </div>
                 </div>
+                <div className='experience-col'>
+                    <div className='experience-row-left'>
+                        <div className='experience-col-date'>
+                            <p>March 2023 - July 2023</p>
+                        </div>
+                    </div>
+                    <div className='experience-row-mid'>
+                        <div className='experience-vertln'>
+                        </div>
+                        <div className='experience-circesh'>
+                        </div>
+                        <div className='experience-vertln'>
+                        </div>
+                    </div>
+                    <div className='experience-row-right'>
+                        <div className='experience-col-desc'>
+                            <div className='experience-desc-heading'>
+                                <div className='experience-desc-header'>
+                                    <p>Web Developer</p>
+                                </div>
+                                <div className='experience-desc-subheader'>
+                                    <p className='experience-company'>LexMeet, Inc.</p>
+                                    <p className='experience-desc'>Internship</p>
+                                </div>
+                            </div>
+                            <div className='experience-desc-body'>
+                                <p>Created interactive ReactJS front-end interfaces, collaborated on Figma mockup conversion, utilized Trello for project management, and maintained code integrity with Git version control.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-col'>
+                    <div className='experience-row-left'>
+                        <div className='experience-col-date'>
+                            <p>March 2023 - July 2023</p>
+                        </div>
+                    </div>
+                    <div className='experience-row-mid'>
+                        <div className='experience-vertln'>
+                        </div>
+                        <div className='experience-circesh'>
+                        </div>
+                        <div className='experience-vertln'>
+                        </div>
+                    </div>
+                    <div className='experience-row-right'>
+                        <div className='experience-col-desc'>
+                            <div className='experience-desc-heading'>
+                                <div className='experience-desc-header'>
+                                    <p>Web Developer</p>
+                                </div>
+                                <div className='experience-desc-subheader'>
+                                    <p className='experience-company'>LexMeet, Inc.</p>
+                                    <p className='experience-desc'>Internship</p>
+                                </div>
+                            </div>
+                            <div className='experience-desc-body'>
+                                <p>Created interactive ReactJS front-end interfaces, collaborated on Figma mockup conversion, utilized Trello for project management, and maintained code integrity with Git version control.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-col'>
+                    <div className='experience-row-left'>
+                        <div className='experience-col-date'>
+                            <p>March 2023 - July 2023</p>
+                        </div>
+                    </div>
+                    <div className='experience-row-mid'>
+                        <div className='experience-vertln'>
+                        </div>
+                        <div className='experience-circesh'>
+                        </div>
+                        <div className='experience-vertln'>
+                        </div>
+                    </div>
+                    <div className='experience-row-right'>
+                        <div className='experience-col-desc'>
+                            <div className='experience-desc-heading'>
+                                <div className='experience-desc-header'>
+                                    <p>Web Developer</p>
+                                </div>
+                                <div className='experience-desc-subheader'>
+                                    <p className='experience-company'>LexMeet, Inc.</p>
+                                    <p className='experience-desc'>Internship</p>
+                                </div>
+                            </div>
+                            <div className='experience-desc-body'>
+                                <p>Created interactive ReactJS front-end interfaces, collaborated on Figma mockup conversion, utilized Trello for project management, and maintained code integrity with Git version control.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-col'>
+                    <div className='experience-row-left'>
+                        <div className='experience-col-date'>
+                            <p>March 2023 - July 2023</p>
+                        </div>
+                    </div>
+                    <div className='experience-row-mid'>
+                        <div className='experience-vertln'>
+                        </div>
+                        <div className='experience-circesh'>
+                        </div>
+                        <div className='experience-vertln'>
+                        </div>
+                    </div>
+                    <div className='experience-row-right'>
+                        <div className='experience-col-desc'>
+                            <div className='experience-desc-heading'>
+                                <div className='experience-desc-header'>
+                                    <p>Web Developer</p>
+                                </div>
+                                <div className='experience-desc-subheader'>
+                                    <p className='experience-company'>LexMeet, Inc.</p>
+                                    <p className='experience-desc'>Internship</p>
+                                </div>
+                            </div>
+                            <div className='experience-desc-body'>
+                                <p>Created interactive ReactJS front-end interfaces, collaborated on Figma mockup conversion, utilized Trello for project management, and maintained code integrity with Git version control.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='experience-col'>
+                    <div className='experience-row-left'>
+                        <div className='experience-col-date'>
+                            <p>March 2023 - July 2023</p>
+                        </div>
+                    </div>
+                    <div className='experience-row-mid'>
+                        <div className='experience-vertln'>
+                        </div>
+                        <div className='experience-circesh'>
+                        </div>
+                        <div className='experience-vertln'>
+                        </div>
+                    </div>
+                    <div className='experience-row-right'>
+                        <div className='experience-col-desc'>
+                            <div className='experience-desc-heading'>
+                                <div className='experience-desc-header'>
+                                    <p>Web Developer</p>
+                                </div>
+                                <div className='experience-desc-subheader'>
+                                    <p className='experience-company'>LexMeet, Inc.</p>
+                                    <p className='experience-desc'>Internship</p>
+                                </div>
+                            </div>
+                            <div className='experience-desc-body'>
+                                <p>Created interactive ReactJS front-end interfaces, collaborated on Figma mockup conversion, utilized Trello for project management, and maintained code integrity with Git version control.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
       </div>
